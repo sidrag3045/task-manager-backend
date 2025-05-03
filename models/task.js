2e48826e06acaaa18ef226c738c282f8d486dd98
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-04 03:07:25 +0530

    Defining Task model

--- models/task.js
+++ models/task.js
@@ -1,8 +1,28 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+// Defining the Task model
 const Task = sequelize.define('Task', {
-  // fields added in Step 2
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: DataTypes.TEXT,
+  dueDate: DataTypes.DATE,
+  status: {
+    type: DataTypes.ENUM('open','completed'),
+    defaultValue: 'open'
+  },
+  assignedTo: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  }
 });
 
+// Exporting the Task model
 module.exports = Task;
