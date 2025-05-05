8b16d6535d484a23c208c2ef5e0993bfc8dc6650
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-05 20:30:24 +0530

    Implementing createTask endpoint in taskController

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,24 @@
-exports.createTask = (req, res) => res.send('Create task not implemented');
+const { Task } = require('../models');
+
+exports.createTask = async (req, res) => {
+  const { title, description, dueDate, assignedTo } = req.body;
+  if (!title) {
+    return res.status(400).json({ message: 'Title is required.' });
+  }
+  try {
+    const task = await Task.create({
+      title,
+      description,
+      dueDate,
+      assignedTo: assignedTo || req.userId
+    });
+    res.status(201).json(task);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 exports.listTasks = (req, res) => res.send('List tasks not implemented');
 exports.getTask = (req, res) => res.send('Get task not implemented');
 exports.updateTask = (req, res) => res.send('Update task not implemented');
