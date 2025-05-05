0ad9d522095e649e2ca8939debe80e0bc8ab13de
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-05 23:04:18 +0530

    Wiring Team–User (M:N) and Team–Task (1:M) associations

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
 const sequelize = require('../config/database');
 const User = require('./user');
 const Task = require('./task');
+const Team = require('./team');
 
-// Defining Associations
+// User-Task Associations (One-to-Many)
 User.hasMany(Task, { foreignKey: 'assignedTo' });
 Task.belongsTo(User, { as: 'assignee', foreignKey: 'assignedTo' });
 
-module.exports = { sequelize, User, Task };
+// User-Team Associations (Many-to-Many)
+User.belongsToMany(Team, { through: 'TeamMembers' });
+Team.belongsToMany(User, { through: 'TeamMembers' });
+
+// Task-Team Associations (One-to-Many)
+Team.hasMany(Task, { foreignKey: 'teamId' });
+Task.belongsTo(Team, { foreignKey: 'teamId' });
+
+module.exports = { sequelize, User, Task, Team };
