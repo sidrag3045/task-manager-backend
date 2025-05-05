cca98d000edeaedf473969c2a10ff08d85f574ef
Raghav Chanana <raghav.chanana7@gmail.com>
2025-05-05 23:47:39 +0530

    Added Attachment model

---
+++ models/attachment.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Attachment = sequelize.define('Attachment', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  filename: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mimetype: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Attachment;
